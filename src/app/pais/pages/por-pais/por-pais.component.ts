61e6cec5fdce734ae319043b196036e37431d729
enrique palomino <octepc.97@gmail.com>
2023-01-15 19:19:46 -0500

    consumed the API

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
 })
 export class PorPaisComponent {
+  termino:string='Hola mundo'
 
+  constructor(private paisService: PaisService){}
+
+  buscar(){
+    this.termino
+    this.paisService.buscarPais(this.termino)
+      .subscribe(resp=>{
+        console.log(resp)
+      })
+
+  }
 }
+
